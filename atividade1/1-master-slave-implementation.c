b55e1316023766d9eb1e0601195d7fcd5a2eb9bd
megaVE <vinicius.honorio@sou.unifal-mg.edu.br>
2023-09-13 13:56:22 -0300

    First Activity Complete

--- atividade1/1-master-slave-implementation.c
+++ atividade1/1-master-slave-implementation.c
@@ -2,17 +2,6 @@
 * Autor: Vinícius Eduardo de Souza Honório
 * Matrícula: 2021.1.08.024
 * ---------------------------------------------------- */
- 
-/*
-* Exercício
-* Criar um modelo master/slave onde o processo 0 é o master que "dispara" tarefas diversificadas para os slaves. Os slaves saberão qual
-* tarefa realizar pelo valor do TAG recebido. Assim que um slave terminar a tarefa, o master deverá atribuir uma nova tarefa
-* TAG=0 -> Soma dos valores
-* TAG=1 -> Média dos valores
-* TAG=2 -> Maior valor
-* TAG=3 -> Mediana dos valores
-* TAG=10 -> Finalizar
-*/
 
 /* COMPILE AND RUNNING INSTRUCTIONS [REQUIRES MPI] */
 
@@ -28,6 +17,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h> // For Linux
+// #include <windows.h> // For Windows
 
 /* GLOBAL VALUES */
 
@@ -40,8 +31,63 @@
 #define MIN_TAG 0
 #define MAX_TAG 3
 
-#define MIN_TASKS 10
-#define MAX_TASKS 20
+#define MIN_TASKS 5
+#define MAX_TASKS 5
+
+#define MIN_SLEEP 1
+#define MAX_SLEEP 1
+
+/* GENERAL FUNCTIONS */
+
+// generates a random int value from the numeric set [min, max]
+int getRNG(int min, int max){
+  return (rand() / (float)RAND_MAX) * (max-min) + min;
+}
+
+// generates a random number of tasks
+int setTasks(){
+  return getRNG(MIN_TASKS, MAX_TASKS);
+}
+
+// generates a random array size
+int setArraySize(){
+  return getRNG(MIN_NUMBERS, MAX_NUMBERS);
+}
+
+// generates a random tag
+int setTag(){
+  return getRNG(MIN_TAG, MAX_TAG);
+}
+
+// generates a random value
+int setValue(){
+  return getRNG(MIN_VALUE, MAX_VALUE);
+}
+
+// generates a random sleep time for slaves
+void slaveRest(){
+  sleep(getRNG(MIN_SLEEP, MAX_SLEEP));
+}
+
+// sets an array with random numbers
+void setArrayRNG(int *array, int size){
+  for(int i=0; i < size; i++)
+    array[i] = setValue();
+}
+
+// sets the next the slave
+int setSlave(int slave, int amount){
+  if(slave < amount)
+    return ++slave;
+  return 1;
+}
+
+// returns the lesser integer value
+int minInt(int a, int b){
+  if(a > b)
+    return b;
+  return a;
+}
 
 /* TASKS FUNCTIONS */
 
@@ -96,59 +142,33 @@ float setOperation(int tag, int *data, int size){
     case 3:
       return medianValues(data, size);
 
-    case 10:
-      return -1;
-
     default:
-      return -10;
+      return -1;
   }
 }
 
-/* GENERAL FUNCTIONS */
+// creates a random task and sends it to a slave
+void createAndSendTask(int *array, int slave, int task){
+  int tag = setTag();
+  int size = setArraySize();
+  setArrayRNG(array, size);
 
-// generates a random int value from the numeric set [min, max]
-int getRNG(int min, int max){
-  return (rand() / (float)RAND_MAX) * (max-min) + min;
+  printf("> MASTER: Slave nº%d will be assigned with task #%d: a tag %d task and a %d sized array\n", slave, task, tag, size);
+  MPI_Send(array, size, MPI_INT, slave, tag, MPI_COMM_WORLD);
 }
 
-// generates a random number of tasks
-int setTasks(){
-  return getRNG(MIN_TASKS, MAX_TASKS);
-}
+// receives a task from a slave, prints the result and returns the sender
+void receiveAndPrintResult(MPI_Status * status){
+  float slave_response;
 
-// generates a random array size
-int setArraySize(){
-  return getRNG(MIN_NUMBERS, MAX_NUMBERS);
-}
-
-// generates a random tag
-int setTag(){
-  return getRNG(MIN_TAG, MAX_TAG);
-}
-
-// generates a random value
-int setValue(){
-  return getRNG(MIN_VALUE, MAX_VALUE);
-}
-
-// sets an array with random numbers
-void setArrayRNG(int *array, int size){
-  for(int i=0; i < size; i++)
-    array[i] = setValue();
-}
-
-// sets the next the slave
-int setSlave(int slave, int amount){
-  if(slave < amount)
-    return ++slave;
-  return 1;
+  MPI_Recv(&slave_response, 1, MPI_FLOAT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, status);
+  printf("> MASTER: %f result received from slave %d\n", slave_response, status->MPI_SOURCE);
 }
 
 /* ---------------------------------------------------- */
 
 int main(int argc, char** argv) {
   // program init
-  MPI_Status status;
   MPI_Init(NULL, NULL);
 
   // RNG seed start
@@ -167,51 +187,59 @@ int main(int argc, char** argv) {
   // numbers: task array of values
   int numbers[MAX_NUMBERS];
   
-  const int tasks = 2; //setTasks()
+  const int tasks = setTasks();
   
-  int tag, size;
+  MPI_Status status;
   
   if (world_rank == 0) {
     // Master Process
-    printf("%d tasks created for %d slaves to perform\n", tasks, world_size-1);
+    printf("> MASTER: %d tasks created for %d slaves to perform\n", tasks, world_size-1);
 
-    int slave = 0;    
-    for(int task_number = 0; task_number < tasks; task_number++){ // For each slave
-      slave = setSlave(slave, world_size-1);
-      tag = setTag();
-      size = setArraySize();
-      setArrayRNG(numbers, size);
-      printf("Slave nº%d will be assigned with task #%d: a tag %d task and a %d sized array\n", slave, task_number, tag, size);
+    int current_task = 0;
+    for(; current_task < minInt(tasks, world_size-1); current_task++) // Initial task set
+      createAndSendTask(numbers, current_task+1, current_task);
 
-      MPI_Send(numbers, size, MPI_INT, slave, tag, MPI_COMM_WORLD);
+    for(; current_task < tasks; current_task++){ // Resending tasks
+      receiveAndPrintResult(&status);
+      createAndSendTask(numbers, status.MPI_SOURCE, current_task);
     }
 
-    // for(int p = 1; p < world_size; p++) // Ends task sending
-    //   MPI_Send(0, 1, MPI_INT, p, 10, MPI_COMM_WORLD);
-    // for(int p = 1; p < world_size; p++){
-    //   MPI_Recv(&somaproc, 1, MPI_INT, p, 5, MPI_COMM_WORLD, &status);
-    //   soma += somaproc;
-    // }
-    // printf("A soma total foi %d e a soma local foi %d\n", soma, somalocal);
-    printf("All tasks finished! Shutting down...\n");
+    current_task = 0;
+    for(; current_task < minInt(tasks, world_size-1); current_task++) // Receiving last tasks
+      receiveAndPrintResult(&status);
+    
+    for(int slave=1; slave < world_size; slave++) // Turns slaves off
+      MPI_Send(&slave, 1, MPI_INT, slave, 10, MPI_COMM_WORLD);
+    
+    if(current_task == minInt(tasks, world_size-1))
+      printf("> MASTER: All tasks finished! Shutting down...\n");
   } else {
     // slave Process
-    int currentSize, currentTag;
-    MPI_Recv(numbers, MAX_NUMBERS, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-    currentTag = status.MPI_TAG;
-    MPI_Get_count(&status, MPI_INT, &currentSize);
     
-    printf("Task received from slave nº%d with tag %d and %d length\n", world_rank, currentTag, currentSize);
+    int current_size, current_tag;
+    while(1){
+      MPI_Recv(numbers, MAX_NUMBERS, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-    float returnValue = setOperation(currentTag, numbers, currentSize);
-    MPI_Send(&returnValue, 1, MPI_FLOAT, 0, currentTag, MPI_COMM_WORLD);
-    printf("Slave nº%d obtained %f as the result of their task and sent back to master\n", world_rank, returnValue);
+      current_tag = status.MPI_TAG;
+
+      if(current_tag == 10){ // final tag
+        printf("> SLAVE: Slave n%d finished all their tasks. Shutting down...\n", world_rank);
+        break;
+      }
+
+      MPI_Get_count(&status, MPI_INT, &current_size);
+      
+      printf("> SLAVE: Task received from slave nº%d with tag %d and %d length\n", world_rank, current_tag, current_size);
+
+      float returnValue = setOperation(current_tag, numbers, current_size);
+      slaveRest();
+      
+      printf("> SLAVE: Slave nº%d obtained %f as the result of their task and sent back to master\n", world_rank, returnValue);
+      MPI_Send(&returnValue, 1, MPI_FLOAT, 0, current_tag, MPI_COMM_WORLD);
+    }
   }
 
   // program shutdown
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
 }
-
-
